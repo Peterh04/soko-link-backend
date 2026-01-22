65d7113403ea4d5f1ae6bf9f4cbe64bd0addd4b7
Peter Heri <peterheri@Peters-MacBook-Pro.local>
2026-01-22 21:05:58 +0300

    refactor(auth):store access token in memory instead of localstorage

--- controllers/authController.js
+++ controllers/authController.js
@@ -71,7 +71,7 @@ export const login = async (req, res) => {
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      secure: true,
+      secure: process.env.NODE_ENV === "production",
       sameSite: "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
