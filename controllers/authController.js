2f7d7ba0ae097dd67de910e14321f2f0245162ba
Peter Heri <peterheri@Peters-MacBook-Pro.local>
2026-01-23 11:35:22 +0300

    refactor(auth):store access token in memory instead of localstorage

--- controllers/authController.js
+++ controllers/authController.js
@@ -71,7 +71,7 @@ export const login = async (req, res) => {
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       sameSite: "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
--- server.js
+++ server.js
@@ -21,12 +21,7 @@ const port = process.env.PORT || 8001;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "https://soko-link.vercel.app/",
-    credentials: true,
-  }),
-);
+app.use(cors());
 app.use("/uploads", express.static("uploads"));
 app.use(express.urlencoded({ extended: true }));
 
