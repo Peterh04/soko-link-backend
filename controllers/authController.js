48880388b7e5eeea87b89842ee6da6f7f71a7f65
Peter Heri <peterheri@Peters-MacBook-Pro.local>
2026-01-23 11:59:47 +0300

    refactor(logout): Logging out now clears the refresh token in the cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -72,7 +72,7 @@ export const login = async (req, res) => {
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
